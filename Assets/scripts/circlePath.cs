6bbd7ac4b84dbcc657cd9d4504b1ee4cd6b31297
madhuracharya <madhur.acharya@opscale.io>
2020-08-16 21:00:50 +0530

    implemented item adition with correct indexing and circle vertex update.

--- Assets/scripts/circlePath.cs
+++ Assets/scripts/circlePath.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(LineRenderer))]
 public class circlePath : MonoBehaviour
@@ -10,13 +11,20 @@ public class circlePath : MonoBehaviour
 	public float radius= 1f;
 	public GameObject obj;
 	public GameObject theLine;
+	public Transform panel;
 
 	private LineRenderer lineRenderer;
 	private List<GameObject> objectList= new List<GameObject>();
 	private Vector3 center;
 
-	public float touchAngle= 0f;
-	public int touchIndex= 1;
+	private float touchAngle= 0f;
+	private float actualAngle= 0f;
+	private int touchIndex= 0;
+	private float currentIndex= 0;
+
+	private float touchRate= 0.25f;
+	private float canTouch= -1f;
+	private Vector3 touchPos;
 
 	void Awake()
 	{
@@ -25,14 +33,16 @@ public class circlePath : MonoBehaviour
 
 	void Start()
 	{
-		EventSystem.current.onTouchTrigger+= addItem;
 		center= transform.position;
 
 		for(int i= 0; i < vertexCount; i ++)
 		{
 			GameObject circle= Instantiate(obj, new Vector3(0, 0, 0), transform.rotation); 
 			circle.transform.SetParent(transform);
-			circle.GetComponent<SpriteRenderer>().color= new Color( Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+			circle.transform.SetParent(panel);
+			circle.GetComponent<RectTransform>().localScale= new Vector3(1f, 1f, 1f);
+			circle.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text= i + "";
+			circle.GetComponent<Image>().color= new Color( Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
 			objectList.Add(circle);
 		}
 
@@ -49,19 +59,36 @@ public class circlePath : MonoBehaviour
 
 		float delta = (2f * Mathf.PI) / (vertexCount);
 
-		if(touch.phase == TouchPhase.Moved)
+		if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Began)
 		{
-			float ang= Mathf.Atan2(touchPos.y, touchPos.x);
-			touchAngle= ang;
-			float aproxIndex= ang / delta;
+			lineRenderer.enabled= true;
+			touchAngle= Mathf.Atan2(touchPos.y, touchPos.x);
+			actualAngle= (touchAngle >= 0 ? touchAngle : ((Mathf.PI * 2) + touchAngle)) * Mathf.Rad2Deg;
 
-			Debug.DrawLine (transform.position, new Vector3(Mathf.Cos(ang), Mathf.Sin(ang)) * radius);
+			float aproxIndex= touchAngle / delta;
+			Debug.DrawLine (transform.position, new Vector3(Mathf.Cos(touchAngle), Mathf.Sin(touchAngle)) * radius);
 
-			ang= (Mathf.Floor(aproxIndex) * delta) + (delta / 2);
+			touchAngle= (Mathf.Floor(aproxIndex) * delta) + (delta / 2);
+			lineRenderer.SetPosition(1, new Vector3(Mathf.Cos(touchAngle), Mathf.Sin(touchAngle)) * radius);
 			
-			lineRenderer.SetPosition(1, new Vector3(Mathf.Cos(ang), Mathf.Sin(ang)) * radius);
-			touchAngle= (ang * Mathf.Rad2Deg);
-			touchIndex= (int)Mathf.Floor(aproxIndex);
+			for(int i= 0; i <= vertexCount; i++)
+			{
+				currentIndex= i * delta * Mathf.Rad2Deg;
+				if(actualAngle <= (i * delta * Mathf.Rad2Deg))
+				{
+					touchIndex= i;
+					break;
+				}
+			}
+		}
+
+		if(Input.touchCount > 0 && Time.time > canTouch)
+		{	
+			if(touch.phase == TouchPhase.Ended)
+			{
+				addItem();
+				canTouch= Time.time + touchRate;
+			}
 		}
 	}
 
@@ -78,22 +105,29 @@ public class circlePath : MonoBehaviour
 		}
 	}
 
-	public void addItem(Vector3 touchPos)
+	public void addItem()
 	{
+		lineRenderer.enabled= false;
+
 		GameObject circle= Instantiate(obj, new Vector3(0, 0, 0), transform.rotation); 
-		circle.GetComponent<SpriteRenderer>().color= new Color( Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+		circle.GetComponent<Image>().color= new Color( Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
 		circle.transform.SetParent(transform);
-		objectList.Insert((touchIndex < 0 ? objectList.Count + touchIndex : touchIndex), circle);
-		//transform.rotation= Quaternion.Euler(new Vector3(0, 0, touchAngle));
+		circle.transform.SetParent(panel);
+		circle.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text= objectList.Count + "";
+		circle.GetComponent<RectTransform>().localScale= new Vector3(1f, 1f, 1f);
+
+		if(touchIndex >= objectList.Count)
+			objectList.Add(circle);
+		else
+			objectList.Insert(touchIndex, circle);
 
 		vertexCount++;
-
 		updatePoints();
 	}
 
 	public void OnDestroy()
 	{
-		EventSystem.current.onTouchTrigger+= addItem;
+		
 	}
 
 	#if UNITY_EDITOR
